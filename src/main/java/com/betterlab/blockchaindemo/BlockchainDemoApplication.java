aff9d444f7e0efd560d0cc9e7c89090eba1a5350
Victor Arley Perez Blanco <victorarley35@gmail.com>
2022-01-02 19:11:03 +0100

    Initial commit

---
+++ src/main/java/com/betterlab/blockchaindemo/BlockchainDemoApplication.java
@@ -0,0 +1,13 @@
+package com.betterlab.blockchaindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betterlab/blockchaindemo/BlockchainDemoApplicationTests.java
@@ -0,0 +1,72 @@
+package com.betterlab.blockchaindemo;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.betterlab.blockchaindemo.entities.Block;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BlockchainDemoApplicationTests {
+
+    public static List<Block> blockchain = new ArrayList<Block>();
+    public static int prefix = 4;
+    public static String prefixString = new String(new char[prefix]).replace('\0', '0');
+
+    @BeforeClass
+    public static void setUp() {
+        Block genesisBlock = new Block("The is the Genesis Block.", "0", new Date().getTime());
+        genesisBlock.mineBlock(prefix);
+        blockchain.add(genesisBlock);
+        Block firstBlock = new Block("The is the First Block.", genesisBlock.getHash(), new Date().getTime());
+        firstBlock.mineBlock(prefix);
+        blockchain.add(firstBlock);
+    }
+
+    @Test
+    public void givenBlockchain_whenNewBlockAdded_thenSuccess() {
+        Block newBlock = new Block("The is a New Block.", blockchain.get(blockchain.size() - 1)
+                .getHash(), new Date().getTime());
+        newBlock.mineBlock(prefix);
+        assertTrue(newBlock.getHash()
+                .substring(0, prefix)
+                .equals(prefixString));
+        blockchain.add(newBlock);
+    }
+
+    @Test
+    public void givenBlockchain_whenValidated_thenSuccess() {
+        boolean flag = true;
+        for (int i = 0; i < blockchain.size(); i++) {
+            String previousHash = i == 0 ? "0"
+                    : blockchain.get(i - 1)
+                    .getHash();
+            flag = blockchain.get(i)
+                    .getHash()
+                    .equals(blockchain.get(i)
+                            .calculateBlockHash())
+                    && previousHash.equals(blockchain.get(i)
+                    .getPreviousHash())
+                    && blockchain.get(i)
+                    .getHash()
+                    .substring(0, prefix)
+                    .equals(prefixString);
+            if (!flag)
+                break;
+        }
+        assertTrue(flag);
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        blockchain.clear();
+    }
+
+}
